ddc6cb9304a25af12083e32211b7e75010740da0
Antonio Reid <read.musik@gmail.com>
2018-04-12 21:09:15 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < Sinatra::Base
 
   set :views, Proc.new { File.join(root, "../views/") }
   register Sinatra::Twitter::Bootstrap::Assets
+
+  get '/applicaton' do
+    erb :'applicaton/root'
 end
