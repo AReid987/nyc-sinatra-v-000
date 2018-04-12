8519adaabb76d13483f80f352254d2895c631160
Antonio Reid <read.musik@gmail.com>
2018-04-12 18:38:51 +0000

    automatically backed up by learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -3,6 +3,7 @@ class CreateLandmarks < ActiveRecord::Migration[4.2]
     create_table :landmarks do |t|
       t.string :name
       t.integer :figure_id 
+      t.integer :year_completed
     end
   end
 end
