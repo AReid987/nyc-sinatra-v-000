44b27628d93787592047a79c8665075a2891b651
Antonio Reid <read.musik@gmail.com>
2018-04-12 22:04:32 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -6,7 +6,11 @@ class FiguresController < ApplicationController
 
   get '/figures/new' do
     @landmarks = Landmark.all
-    @titles = Title.all 
+    @titles = Title.all
     erb :'figures/new'
   end
+
+  post '/figures' do
+  end
+  
 end
