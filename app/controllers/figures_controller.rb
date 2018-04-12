28ec18dc42ffbcc69ed3ee421bc0880c3de3841a
Antonio Reid <read.musik@gmail.com>
2018-04-12 22:22:48 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -13,6 +13,7 @@ class FiguresController < ApplicationController
   post '/figures' do
     if !params[:figure][:title_ids].empty?
       @title = Title.find(params[:figure][:title_ids])
+      @figure = Figure.create(name: params[:figure][:name])
     end
     binding.pry
   end
