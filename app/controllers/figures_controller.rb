c4fc0d12b9dd2685612d259ae2ce3e714530e0da
Antonio Reid <read.musik@gmail.com>
2018-04-13 00:26:07 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -15,15 +15,15 @@ class FiguresController < ApplicationController
     #  @title = Title.find(params[:figure][:title_ids])
     #  @figure = Figure.create(name: params[:figure][:name], titles: @title)
     #end
-    title = Title.find(params[:figure][:title_ids])
-    Title.all.each do |title|
-
-      if title
-        @title = title
-        @figure = Figure.create(name: params[:figure][:name])
-        @figure.titles << @title
+    if params[:title][:name].empty?
+      title = Title.find(params[:figure][:title_ids])
+      Title.all.each do |title|
+        if title
+          @title = title
+          @figure = Figure.create(name: params[:figure][:name])
+          @figure.titles << @title
+        end
       end
-    end
     #@landmark = Landmark.find(params[:figure][:landmark_ids])
     binding.pry
     #@figure = Figure.create(name: params[:figure][:name], titles: @title)
