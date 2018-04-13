53fb6b2f7f079f5106180918b59d8c85c18c74cd
Antonio Reid <read.musik@gmail.com>
2018-04-13 01:14:20 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -21,14 +21,13 @@ class FiguresController < ApplicationController
         end
       end
     end
-    binding.pry 
+    binding.pry
     if !params[:figure][:landmark_ids].nil?
       Landmark.all.each do |landmark|
-
-        #if landmark.id.to_s == params[:figure][:landmark_ids].join
-        #  @landmark = landmark
-        #  @figure.landmarks << @landmark
-        #end
+        if landmark.id.to_s == params[:figure][:landmark_ids].join
+          @landmark = landmark
+          @figure.landmarks << @landmark
+        end
       end
     end
 
