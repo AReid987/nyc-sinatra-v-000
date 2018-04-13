4acd0612ea32e5c3161b9e4491795b05f6da1d41
Antonio Reid <read.musik@gmail.com>
2018-04-13 00:48:35 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -19,10 +19,10 @@ class FiguresController < ApplicationController
 binding.pry
     if !params[:title][:title_ids].empty?
         @title = Title.find(params[:figure][:title_ids])
-        @figure.titles << @title    
+        @figure.titles << @title
     end
     #@landmark = Landmark.find(params[:figure][:landmark_ids])
-    
+
     #@figure = Figure.create(name: params[:figure][:name], titles: @title)
 
   end
